028a6ec6cdbe163b0e33928c3a6c5f154684f1d3
training-dev <contact@training-dev.fr>
2026-02-17 14:53:12 +0100

    exo dom create

--- js/DOM/index.js
+++ js/DOM/index.js
@@ -1,4 +1,15 @@
-let element = document.querySelector('.item');
+let user = {
+    name: "Alex Martin",
+    job: "Web Developer",
+    bio: "Passionate about coding and creating user-friendly websites. Loves coffee and travel.",
+    data: {
+        follower: "2.5k",
+        like: 350,
+        joined: 2020 
+    }
+};
+
+let element = document.querySelector('.card');
 
 let price = document.createElement('div');
 price.innerHTML = "13.95€";
